5d264d8ca18c10c9403ddab6af3a4579b404a634
swas02 <60228256+swas02@users.noreply.github.com>
2025-04-19 03:09:54 +0530

    Add files via upload

--- deviceCheck.js
+++ deviceCheck.js
@@ -5,25 +5,19 @@ window.addEventListener('load', function() {
     const deviceWidth = window.innerWidth;
   
     // Check if the current page is the homepage (/) or m.html
-    if (currentPage === '/' || currentPage === '/m.html') {
+    if (currentPage === '/PromptToHTML' || currentPage === 'PromptToHTML/m.html') {
       // If the window width is less than or equal to 540px (mobile)
       if (deviceWidth <= 540) {
         // Redirect to m.html if not already there
-        if (currentPage !== '/m.html') {
-          window.location.href = "/m.html";
+        if (currentPage !== 'PromptToHTML/m.html') {
+          window.location.href = "PromptToHTML/m.html";
         }
       }
-      // If the window width is between 541px and 768px (tablet)
-      else if (deviceWidth > 540 && deviceWidth <= 768) {
-        // You can choose to do something specific for tablets if needed
-        // Currently, it doesn't redirect, but you can add actions for tablet
-        console.log('Device is in tablet range: ', deviceWidth);
-      }
       // If the window width is greater than 768px (desktop)
-      else if (deviceWidth > 768) {
+      else if (deviceWidth > 540) {
         // Redirect to index.html (/) if not already there
-        if (currentPage !== '/') {
-          window.location.href = "/";
+        if (currentPage !== '/PromptToHTML') {
+          window.location.href = "/PromptToHTML";
         }
       }
     }
